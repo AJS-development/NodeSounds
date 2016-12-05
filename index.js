83818e428f55dc5ec6fab15d64585a7ee3db6f35
Andrew S <andrews54757@gmail.com>
2016-12-05 16:29:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,37 +11,16 @@ module.exports = class Sounds {
         this.getPlayer()
     }
     getPlayer() {
-        switch (this.platform) {
-        case "win32":
-            this.player = "start"
-            break;
-        case "darwin":
-            this.player = "afplay";
-            break;
-
-        }
+ 
     }
     play(name) {
-        if (this.sounds[name]) return this._play(this.sounds[name])
-        return this._play(name)
+     
     }
     _play(dir) {
-        if (!this.player) return false;
-        try {
-        var a = child.spawn(this.player, [dir], {
-            cwd: this.cwd,
-            PATH: process.env.PATH
-        })
-a.on('error',function() {
-    
-})
-        } catch (e) {
-            
-        }
-        return true;
+ 
     }
     add(name, dir) {
-        this.sounds[name] = dir
+
 
     }
     clear() {
